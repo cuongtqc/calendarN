621a675ab9d7bf63b880b7d25f355c3e65fefbed
quangcuong0808 <cuongtq_58@vnu.edu.vn>
2016-04-04 13:50:06 +0700

    prepare for add and edit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,9 @@
+// function hệ thống
+ //var jsdom = require('jsdom').jsdom;
+ //var document = jsdom('<html></html>', {});
+ //var window = document.defaultView;
+ //var $ = require('jquery')(window);
+ 
 $(function() {
 
     $('#login-form-link').click(function(e) {
@@ -17,14 +23,63 @@ $(function() {
 	});
 
 });
-// bên trên là jquery cho phần login cho đẹp
 
-// user data( template )
-//var user = jQuery.parseJSON( ""+document.getElementById('userinfo').value );
-//$("#avatar").attr("src", user.avatar );
-//console.log("Userinfor: " + document.getElementById('userinfo').value);
-//$("#username").html( user.name );
-function logout() {
-	window.location = "/logout";
+var differ = function( date1 , date2 ) {
+	var one_day = 1000*60*60*24;
+	var date1_ms = date1.getTime();
+	var date2_ms = date2.getTime();
+	var difference_ms = date2_ms - date1_ms;
+	return Math.round( difference_ms / one_day ); 
 }
 
+// user data( template )
+// user là JSON object
+//{ "name": "John", "avatar": "img/avatar.jpg"}
+var user = jQuery.parseJSON( $("userinfo").text());
+$("#avatar").attr("src", sess.user.avatar );
+$("#username").html( user.name );
+
+
+// task data( template )
+// task là JSON array chứa các task và deadline
+var data = user.task;
+console.log( data );
+
+function test(){
+	console.log('Test call js function');
+}
+// khi nào get được json thì chạy đống dưới đây
+// select day function sẽ được thêm sau, bây giờ cứ làm màu đã :v
+$("#task-list").empty();
+var toDay = new Date(); toDay.setHours( 0 , 0 , 0 , 0 );
+$.each( data, function( i, item ) {
+	var from = item.deadline.split( '-' );
+	var day = new Date( from[2], from[1] - 1, from[0] );
+	var x = differ( toDay , day );
+	if ( x > 3 ) x = 3; x *= 25;
+	var htmlData = '<li class="task-item">' + 
+			'<div class="progress">' + 
+				'<div class="progress-bar progress-bar-info progress-bar-striped active" style="width:' + x + '%">' +
+					item.content + 
+				'</div>' + 
+			'</div>' 
+		'</li>';
+	$("#task-list").append( htmlData );
+})
+
+// đến đây là hết cái đống get json rồi
+
+
+$("#task-deadline").pickadate({
+	today: '',
+	clear: '',
+	close: 'Cancel',
+	min: 1
+})
+
+//Cuong them function
+function logout(){
+	window.location = 'http://localhost:3000/logout';
+}
+
+//module.exports = {foo: test};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var session = require('express-session');
 var mongodb = require('mongodb');
+//var clientjs = require(__dirname.slice(0, __dirname.length - 6) +'\\public\\javascripts\\app.js');
 
 var db = require('monk')('mongodb://quangcuong0808:kiwihoabattu95@ds011790.mlab.com:11790/cdcnapp');
 var router = express.Router();
@@ -13,43 +14,39 @@ app.use(session({secret: 'duanaohachduocpassnayAhihi'}));
 
 
 /* GET home page. */
-var sess; //session variable
+var sess = {}; //session variable
+sess.user = sess.user||{};
 
 router.get('/', function(req, res, next) {
 	sess = req.session;
-	//console.log("Logged = " + sess.logged);
-	//console.log("Name = " + sess.name);
-	var user = {};
-	if (sess.logged) {
-		user.name = sess.name || "Guest";
-		user.logged = 1;
+	sess.user = sess.user||{};
+	if (JSON.stringify(sess.user)!='{}' && sess.user!=null) {
+		sess.user.logged = 1;
 	} else {
-		user.name = sess.name||"Guest";
-		user.logged = 0;
-	};	
-	res.render('index', {user: user});
+		sess.user.logged = 0;
+	};
+	sess.user.act = 'view';
+	sess.user.name = sess.user.name||"Guest";	
+	res.render('index', {sess: sess});
 });
 
 router.post('/login', function(req, res, next){
 	sess = req.session;
-	users.find( { username:req.body.username, password:req.body.password}, function(err, docs){
+	sess.user = sess.user||{};
+	var promise = users.find( { username:req.body.username, password:req.body.password}, function(err, docs){
 		if (err) { 
 			console.log(err)
 		}
 		else {
 			//Chưa có task nên là chưa lấy task về
-			if (req.body.remember && docs) {
-				for(var i = 0; i < docs.length; i++){
-					sess.name = docs[i].name;
-					sess.logged = 1;
-					sess.username = docs[i].username;
-					sess.password = docs[i].password;
-				}
-			}
 			if (docs) {
 				for(var i = 0; i < docs.length; i++){
-					sess.name = docs[i].name;
-					sess.logged = 1;
+					sess.user.name = docs[i].name;
+					sess.user.logged = 1;
+					sess.user.act = 'view';
+					sess.user.username = docs[i].username;
+					sess.user.password = docs[i].password;
+					sess.user.task = docs[i].task;
 				}
 			};
 			res.redirect("/");	
@@ -62,8 +59,24 @@ router.get('/logout', function(req, res, next){
 	req.session.destroy();
 	res.redirect('/');
 });
+
 router.get('/register', function(req, res, next){
-	users.insert({username:"quangcuong0808", password:"123", name:"Cương Trần"});
+	//insert user
+	//users.insert({username:"quangcuong0808", password:"123", name:"Cương Trần"});
+	//edit database
+	users.findAndModify({username : 'quangcuong0808'}, 
+		{ $set: {task : [{content:"Going to school", deadline:'12-1-2012'}]}} ,
+		{upsert : true},
+		function (err, result) {
+    	console.log(err);
+	});
+});
+
+router.get('/act:add', function(req, res, next){
+	var add = req.params.add;
+	sess.user = sess.user||{};
+	sess.user.act = add;
+	res.redirect('/');
 });
 
 module.exports = router;
