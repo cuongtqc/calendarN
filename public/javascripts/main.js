f765791aac3db6b198ebfdd000f08acb583f621e
quangcuong0808 <cuongtq_58@vnu.edu.vn>
2016-04-10 10:25:17 +0700

    fixed sort

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
 	var task = jQuery.parseJSON( $("#userinfo").text() ).task;
 	
 	// select day function sẽ được thêm sau, bây giờ cứ làm màu đã :v
-	$("#taskList").empty();
+	$("#task-list").empty();
 	var toDay = new Date(); toDay.setHours( 0 , 0 , 0 , 0 );
 
 	for(i = 0; i<task.length; i++) {
@@ -53,14 +53,14 @@ $(document).ready(function(){
 		var x = differ( toDay , day );
 
 		if ( x > 3 ) x = 3; temp = (x) * 25;
-		var htmlData = '<div class="task-item">' + 
-					'<div class="progress">' + 
-						'<div class="progress-bar progress-bar-info progress-bar-striped active" style="width:' + temp + '%">' + 
-							task[i].content +
-						'</div>' + 
-					'</div>' +
-			'</div>';
-		$("#taskList").append( htmlData );
+		var htmlData = '<li class="task-item" style="list-style-type: none;">' + 
+			'<div class="progress">' + 
+				'<div class="progress-bar progress-bar-info progress-bar-striped active" style="width:' + temp + '%" value="' + differ( toDay, day ) + '">' +
+					task[i].content + 
+				'</div>' + 
+			'</div>' +
+		'</li>';
+		$("#task-list").append( htmlData );
 	};
 
 	// đến đây là hết cái đống get json rồi
@@ -79,7 +79,7 @@ $(document).ready(function(){
 		
 	function sortI(){
 		
-			var items = $("#taskList div").get();
+			var items = $("#task-list li").get();
 			items.sort( function( a , b ) {
 				var keyA = $(a).children(".progress").children().attr( "value" );
 				var keyB = $(b).children(".progress").children().attr( "value" );
@@ -87,7 +87,7 @@ $(document).ready(function(){
 				if ( keyA > keyB ) return 1;
 				return 0;
 			});
-			var ul = $('#taskList');
+			var ul = $('#task-list');
 			$.each(items, function(i, li){
 				ul.append(li);
 			});
@@ -103,3 +103,4 @@ $(document).ready(function(){
 	function edit(){
 		window.location.href = '/edit';
 	}
+	
