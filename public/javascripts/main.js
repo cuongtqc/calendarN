105c42bd75631ed5c2bf463e1d14dccd601cd01c
Duy <duynl_58@vnu.edu.vn>
2016-04-13 21:46:02 +0700

    Select day working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,14 +7,54 @@
 		var difference_ms = date2_ms - date1_ms;
 		return Math.round( difference_ms / one_day ); 
 	};
+	
+	var getTaskListWithDate = function( currentDate ) {
+		$("#task-list").empty();
+		if(task != null){
+			for(i = 0; i<task.length; i++) {
+				var from = task[i].deadline.split( '-' );
+				var day = new Date();
+				if (from[0].length==4) day = new Date( from[0], from[1] - 1, from[2] );	
+				if (from[2].length==4) day = new Date( from[2], from[1] - 1, from[0] );	
+				var x = differ( currentDate , day );
+				if ( x <= 0 ) continue;
+				if ( x > 3 ) x = 3; temp = (x+1) * 25;
+				var htmlData = '<li class="task-item" style="list-style-type: none;" title="Click on this to manage this task.">' + 
+					'<div class="progress" onclick="showtool('+i+')">' + 
+						'<div class="progress-bar progress-bar-info progress-bar-striped active" style="width:' + temp + '%" value="' + differ( toDay, day ) + '">' +
+							task[i].content + 
+						'</div>' + 
+						'</div>' +	
+							'<div id="'+i+'" hidden>'+
+								'<div class="btn" id="cancelformshow'+i+'" onclick="formshow(\'formshow'+i+'\')">Edit</div>'+
+								'<a class="btn "href="/del/'+i+'" >Delete</a>'+
+							'</div>'+
+							'<div id="formshow'+i+'" hidden>'+
+								'<h2>Edit</h2>'+
+								'<form class="task-edit-form" method="post" action="/edit/'+i+'" role="form" style="display: block;">'+
+								'<div class="form-group">'+
+								'<input type="text" name="task_name" tabindex="1" placeholder="Task\'s name" class="form-control"/>'+
+							'</div>'+
+							'<div class="form-group">'+
+								'<input type="date" name="task_deadline" tabindex="2" placeholder="Task\'s deadline" class="form-control deadline-edit"/>'+
+							'</div>'+
+							'<div class="form-group">'+
+								'<input type="submit" name="task-submit" tabindex="3" value="Submit" class=" btn "/>'+
+							'</div>'+
+						'</form>'+
+					'</div>'+
+				'</li>';
+				$("#task-list").append( htmlData );
+			};
+		}
+	};
 
 	var getString = function( date ) {
 		var day = date.getDate();
 		var month = date.getMonth() + 1;
 		var year = date.getFullYear();
 		return day + '/' + month + '/' + year;
-
-	}
+	};
 	
 	$( document ).ready(function() {
 		var date = new Date();
@@ -23,6 +63,8 @@
 		$("#day2").html( getString(new Date(date.getTime()+2*dayPlus)) );
 		$("#day3").html( getString(new Date(date.getTime()+3*dayPlus)) );
 		$("#selectDay").attr( "placeholder", getString( date ) );
+		var toDay = new Date(); toDay.setHours( 0 , 0 , 0 , 0 );
+		getTaskListWithDate( toDay );
 	});
 		
 	// user là JSON object
@@ -35,50 +77,12 @@
 	var task = jQuery.parseJSON( $("#userinfo").text() ).task;
 	
 	// select day function sẽ được thêm sau, bây giờ cứ làm màu đã :v
-	$("#task-list").empty();
-	var toDay = new Date(); toDay.setHours( 0 , 0 , 0 , 0 );
-	if(task != null){
-		for(i = 0; i<task.length; i++) {
-			var from = task[i].deadline.split( '-' );
-			var day = new Date();
-		
-			if (from[0].length==4) day = new Date( from[0], from[1] - 1, from[2] );	
-		
-			if (from[2].length==4) day = new Date( from[2], from[1] - 1, from[0] );	
-			
-			
-			var x = differ( toDay , day );
-			
-			if ( x > 3 ) x = 3; temp = (x+1) * 25;
-			var htmlData = '<li class="task-item" style="list-style-type: none;" title="Click on this to manage this task.">' + 
-				'<div class="progress" onclick="showtool('+i+')">' + 
-					'<div class="progress-bar progress-bar-info progress-bar-striped active" style="width:' + temp + '%" value="' + differ( toDay, day ) + '">' +
-						task[i].content + 
-					'</div>' + 
-				'</div>' +	
-				'<div id="'+i+'" hidden>'+
-						'<div class="btn" id="cancelformshow'+i+'" onclick="formshow(\'formshow'+i+'\')">Edit</div>'+
-						'<a class="btn "href="/del/'+i+'" >Delete</a>'+
-				'</div>'+
-				'<div id="formshow'+i+'" hidden>'+
-  					'<h2>Edit</h2>'+
-    				'<form class="task-edit-form" method="post" action="/edit/'+i+'" role="form" style="display: block;">'+
-	      				'<div class="form-group">'+
-	        				'<input type="text" name="task_name" tabindex="1" placeholder="Task\'s name" class="form-control"/>'+
-	      				'</div>'+
-	      				'<div class="form-group">'+
-	        				'<input type="date" name="task_deadline" tabindex="2" placeholder="Task\'s deadline" class="form-control deadline-edit"/>'+
-      					'</div>'+
-      					'<div class="form-group">'+
-        					'<input type="submit" name="task-submit" tabindex="3" value="Submit" class=" btn "/>'+
-      					'</div>'+
-    				'</form>'+
-				'</div>'+
-			'</li>';
-			$("#task-list").append( htmlData );
-		};
-	}
-	
+	$("#btn-select").click( function() {
+		var day = new Date();
+		var from = $("#selectDay").val().split( '-' );
+		day = new Date( from[0], from[1] - 1, from[2] );	
+		getTaskListWithDate( day );
+	})
 	
 	$("#task-deadline").pickadate({
 		today: '',
